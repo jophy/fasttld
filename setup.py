3e2501c381211145a46590813777fc168e7cba45
xulichen <xulichen@baidu.com>
2017-06-06 14:33:01 +0800

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fasttld',
-    version='0.1',
+    version='0.1.1',
     packages=['fasttld'],
     url='https://github.com/jophy/fasttld',
     license='GPL',
