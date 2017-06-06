804dc210fb515ed7adf9b6dc8c2839344d692ebf
xulichen <xulichen@baidu.com>
2017-06-06 14:28:46 +0800

    Add sudomain=False tests.

--- tests/performance.py
+++ tests/performance.py
@@ -25,49 +25,68 @@ t1 = time.time()
 t = FastTLDExtract(exclude_private_suffix=False)
 for i in xrange(1, 1000000):
     t.extract(case1, subdomain=True)
-print "fasttld in case1: %ss" % (time.time() - t1)
+print("fasttld in case1: %ss" % (time.time() - t1))
 
 t1 = time.time()
 t = FastTLDExtract(exclude_private_suffix=False)
 for i in xrange(1, 1000000):
     t.extract(case2, subdomain=True)
-print "fasttld in case2: %ss" % (time.time() - t1)
+print("fasttld in case2: %ss" % (time.time() - t1))
 
 t1 = time.time()
 t = FastTLDExtract(exclude_private_suffix=False)
 for i in xrange(1, 1000000):
     t.extract(case3, subdomain=True)
-print "fasttld in case3: %ss\n" % (time.time() - t1)
+print("fasttld in case3: %ss\n" % (time.time() - t1))
+
+# fasttld（subdomain=False） start
+t1 = time.time()
+t = FastTLDExtract(exclude_private_suffix=False)
+for i in xrange(1, 1000000):
+    t.extract(case1, subdomain=False)
+print("fasttld in case1: %ss" % (time.time() - t1))
+
+t1 = time.time()
+t = FastTLDExtract(exclude_private_suffix=False)
+for i in xrange(1, 1000000):
+    t.extract(case2, subdomain=False)
+print("fasttld in case2: %ss" % (time.time() - t1))
+
+t1 = time.time()
+t = FastTLDExtract(exclude_private_suffix=False)
+for i in xrange(1, 1000000):
+    t.extract(case3, subdomain=False)
+print("fasttld in case3: %ss\n" % (time.time() - t1))
 
 
 # tldextract start
 t1 = time.time()
 for i in xrange(1, 1000000):
     tldextract.extract(case1)
-print "tldextract in case1: %ss" % (time.time() - t1)
+print("tldextract in case1: %ss" % (time.time() - t1))
 
 t1 = time.time()
 for i in xrange(1, 1000000):
     tldextract.extract(case2)
-print "tldextract in case2: %ss" % (time.time() - t1)
+print("tldextract in case2: %ss" % (time.time() - t1))
 
 t1 = time.time()
 for i in xrange(1, 1000000):
     tldextract.extract(case3)
-print "tldextract in case3: %ss\n" % (time.time() - t1)
+print("tldextract in case3: %ss\n" % (time.time() - t1))
 
 # tld start
 t1 = time.time()
 for i in xrange(1, 1000000):
     get_tld(case1, fix_protocol=True)
-print "tld in case1: %ss" % (time.time() - t1)
+print("tld in case1: %ss" % (time.time() - t1))
 
 t1 = time.time()
 for i in xrange(1, 1000000):
     get_tld(case1, fix_protocol=True)
-print "tld in case2: %ss" % (time.time() - t1)
+print("tld in case2: %ss" % (time.time() - t1))
 
 t1 = time.time()
 for i in xrange(1, 1000000):
     get_tld(case1, fix_protocol=True)
-print "tld in case3: %ss\n" % (time.time() - t1)
+print("tld in case3: %ss\n" % (time.time() - t1))
